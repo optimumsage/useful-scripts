b90bb387e54481dce55a7a8518aa4cc3ca0e51b3
mohsinsiyal-tsi <mohsin.siyal@technosci.com>
2025-03-28 20:33:18 +0500

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -14,17 +14,17 @@ aliases=(
   "alias sshi='ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i'"
 )
 
-# Function to add aliases to the specified shell config file
-add_aliases_to_shell_config() {
-  local config_file=$1
+# Function to add aliases to a file
+add_aliases_to_file() {
+  local file=$1
   for alias in "${aliases[@]}"; do
-    # Check if the alias is already in the config file
-    if ! grep -Fxq "$alias" "$config_file"; then
+    # Check if the alias is already in the file
+    if ! grep -Fxq "$alias" "$file"; then
       # Add alias if not already present
-      echo "$alias" >> "$config_file"
-      echo "Added alias: $alias to $config_file"
+      echo "$alias" >> "$file"
+      echo "Added alias: $alias to $file"
     else
-      echo "Alias already present in $config_file: $alias"
+      echo "Alias already present in $file: $alias"
     fi
   done
 }
@@ -32,17 +32,35 @@ add_aliases_to_shell_config() {
 # Get the full path of the home directory
 HOME_DIR=$HOME
 
-# Check if Zsh is the current shell
-if [ "$SHELL" = "/bin/zsh" ] || [ "$SHELL" = "/usr/bin/zsh" ]; then
-  echo "Zsh shell detected."
-  add_aliases_to_shell_config "$HOME_DIR/.zshrc"
+# Create or append aliases to .my_aliases file
+MY_ALIASES_FILE="$HOME_DIR/.my_aliases"
+touch "$MY_ALIASES_FILE"
+add_aliases_to_file "$MY_ALIASES_FILE"
+
+# Function to ensure 'source ~/.my_aliases' is added to shell config files
+add_source_to_shell_config() {
+  local config_file=$1
+  if ! grep -Fxq "source ~/.my_aliases" "$config_file"; then
+    echo "source ~/.my_aliases" >> "$config_file"
+    echo "Added 'source ~/.my_aliases' to $config_file"
+  else
+    echo "'source ~/.my_aliases' already present in $config_file"
+  fi
+}
+
+# Check if .zshrc exists, then add 'source ~/.my_aliases' to .bashrc and .zshrc
+if [ -f "$HOME_DIR/.zshrc" ]; then
+  echo ".zshrc file found. Adding source ~/.my_aliases to .zshrc."
+  add_source_to_shell_config "$HOME_DIR/.zshrc"
 else
-  echo "Bash shell detected or another shell is in use."
-  add_aliases_to_shell_config "$HOME_DIR/.bashrc"
+  echo ".zshrc not found, proceeding with .bashrc."
 fi
 
+# Add 'source ~/.my_aliases' to .bashrc
+add_source_to_shell_config "$HOME_DIR/.bashrc"
+
 # Source the relevant config file to apply changes immediately
-if [ "$SHELL" = "/bin/zsh" ] || [ "$SHELL" = "/usr/bin/zsh" ]; then
+if [ -f "$HOME_DIR/.zshrc" ]; then
   source "$HOME_DIR/.zshrc"
 else
   source "$HOME_DIR/.bashrc"
